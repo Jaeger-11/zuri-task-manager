94dffda671515c4110ab09161d6ac22acc762a1b
Falodun Damilola <dpaulzs11@gmail.com>
2022-11-09 18:05:47 +0100

    model changes

--- models/task.js
+++ models/task.js
@@ -1,10 +1,21 @@
 const mongoose = require("mongoose");
 
-const taskSchema = new mongoose.Schema({
+const TaskSchema = new mongoose.Schema({
     title:{
         type: String,
         required: [true, "Must provide a title name"],
         trim: true,
         maxlength: [35, "Title should have no more than 35 characters"]
+    },
+    description:{
+        type:String,
+        required: [true, "Provide a task description"],
+        trim: true,
+    },
+    completed: {
+        type: Boolean,
+        default: false,
     }
-})
+}, {timestamps:true})
+
+module.exports = mongoose.model('Task', TaskSchema);
